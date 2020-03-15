7ed080b7a9477bc6a033a0303fc2063dc79aa9b2
Greg <mail@gregorschulz.eu>
2020-03-15 09:51:34 +0100

    add tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Presentation/React/index.css';
 import App from "./Presentation/React/App";
+import "./Presentation/Css/tailwind.css"
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
