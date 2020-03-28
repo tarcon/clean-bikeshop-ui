439fbaed9823d96d7c8441c188b214f6e9a3d253
Greg <mail@gregorschulz.eu>
2020-03-28 11:18:00 +0100

    add bike class

--- src/Shop/persistence/BikeDatabase.ts
+++ src/Shop/persistence/BikeDatabase.ts
@@ -5,19 +5,23 @@ export type StoredBike = {
 }
 
 export class BikeDatabase {
-
    public static StoredBikes: Array<StoredBike> = [
       {
          name: "Carbono R3",
          price: "4499",
-         description: "A racing bike with a long heritage of classic race wins. Prefered by dentists.",
+         description:
+            "A racing bike with a long heritage of classic race wins. Prefered by dentists.",
       },
       {
          name: "Generalized Asphalt G-Works",
          price: "7999",
-         description: "An even racier bike used by most of the professional riders. Made in china, but priced like artisan work from the USA.",
+         description:
+            "An even racier bike used by most of the professional riders. Made in china, but priced like artisan work from the USA.",
       },
-      { name: "Dungeon Ultra SLX", price: "5699", description: "German engineered racing bike with nice looks." },
+      {
+         name: "Dungeon Ultra SLX",
+         price: "5699",
+         description: "German engineered racing bike with nice looks.",
+      },
    ]
-
 }
