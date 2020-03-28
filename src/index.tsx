4c707d0867c25366ea8023d0f050b683f9a51190
Greg <mail@gregorschulz.eu>
2020-03-28 10:34:04 +0100

    adapt prettier conf and reforamt

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./Presentation/React/App";
-import "./Presentation/Css/tailwind.css";
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./Presentation/React/App"
+import "./Presentation/Css/tailwind.css"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"))
