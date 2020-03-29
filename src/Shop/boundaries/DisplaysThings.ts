5ecc772ddaf9688b6a9ff57feacaa4283a795d1b
Greg <mail@gregorschulz.eu>
2020-03-29 11:07:58 +0200

    fix use case test

--- src/Shop/boundaries/DisplaysThings.ts
+++ src/Shop/boundaries/DisplaysThings.ts
@@ -1,4 +1,4 @@
-import { SeeBikesOutput } from "../use-cases/SeeBikes"
+import { SeeBikesOutput } from "../use-cases/SeeBikesOutput"
 
 export interface DisplaysThings {
    showBikes: (bikesToPresent: SeeBikesOutput) => any
--- src/Shop/use-cases/SeeBikes.test.ts
+++ src/Shop/use-cases/SeeBikes.test.ts
@@ -1,13 +1,20 @@
 import { SeeBikes } from "./SeeBikes"
+import { DisplaysThings } from "../boundaries/DisplaysThings"
+import { ProvidesBikes } from "../boundaries/ProvidesBikes"
 
 
 describe("SeeBikes use case", () => {
    let emptyBikeStorage = {
       fetchPurchasableBikes: jest.fn().mockReturnValue([]),
-   }
+   } as ProvidesBikes
+
+   let ui = {
+      showBikes: jest.fn()
+
+   } as DisplaysThings
 
    it("can be executed", () => {
-      const useCase = new SeeBikes(emptyBikeStorage)
+      const useCase = new SeeBikes(emptyBikeStorage, ui)
 
       expect(() => {
          useCase.execute()
